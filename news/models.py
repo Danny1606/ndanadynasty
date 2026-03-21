97114d9b2d6d62b8b9fa3a7ec34b657f9bbef9f8
Danny1606 <25538@student.embuni.ac.ke>
2026-03-21 18:38:38 +0300

    Update news app views, models, templates, and URLs

--- news/models.py
+++ news/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    body = models.TextField()
     author = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-
-
--- news/views.py
+++ news/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
 def home(request):
-    posts = Post.objects.order_by('-created_at')
-    return render(request, 'news/home.html', {'posts': posts})
+    posts = Post.objects.all().order_by('-created_at')
+    latest_post = posts.first() if posts.exists() else None
+    notifications = posts[:3]  # show latest 3 posts as notifications
+    return render(request, "news/home.html", {
+        "posts": posts,
+        "latest_post": latest_post,
+        "notifications": notifications
+    })
 
 def add_post(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
     else:
         form = PostForm()
-    return render(request, 'news/add_post.html', {'form': form})
+    return render(request, "news/add_post.html", {"form": form})
 
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm(instance=post)
+    return render(request, "news/edit_post.html", {"form": form, "post": post})
 
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('home')
